e1c46acf0f65421ccf62591480497ebf464c5aa5
Winder Ojeda <winder.alejandro@gmail.com>
2016-11-18 19:06:23 -0400

    update service

--- src/main/resources/static/app/services/github.service.js
+++ src/main/resources/static/app/services/github.service.js
@@ -20,11 +20,11 @@ var GithubService = (function () {
         this.username = "ojedawinder";
     }
     GithubService.prototype.getUser = function () {
-        return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+        return this._http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(function (res) { return res.json(); });
     };
     GithubService.prototype.getRepos = function () {
-        return this._http.get('http://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+        return this._http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(function (res) { return res.json(); });
     };
     GithubService.prototype.updateUser = function (username) {
--- src/main/resources/static/app/services/github.service.ts
+++ src/main/resources/static/app/services/github.service.ts
@@ -14,12 +14,12 @@ export class GithubService{
 	}
 
 	getUser(){
-		return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+		return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
 		.map(res => res.json());
 	}
 
 	getRepos(){
-		return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+		return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
 		.map(res => res.json());
 	}
 
